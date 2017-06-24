e70ac6c1778e19d1e8355c5bbb468019329107f4
wtam <william.tam.2013@hotmail.com.hk>
2017-06-24 23:14:44 +0800

    test

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,16 +1,9 @@
-﻿using Autofac;
-using BotAuth.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
-using System.Reflection;
-using System.Runtime;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
-using Autofac.Integration.WebApi;
-
 
 namespace InsuranceBoT
 {
